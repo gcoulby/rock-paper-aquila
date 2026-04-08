60d5a7c1eb5b09e7f21d0860551d27a99c5d0365
Graham <gcoulby@gmail.com>
2026-04-08 21:27:22 +0100

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/rock-paper-aquila/',
   plugins: [react(), tailwindcss()],
 })
